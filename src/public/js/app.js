5798e6387db1be8b9b26d749eb3b5dd6406225c8
DoYounBang <82746064+DoYounBang@users.noreply.github.com>
2021-08-30 12:52:01 +0900

    Fourth commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert ("hi");
+const socket = new WebSocket(`ws://${window.location.host}/`);
+
+socket.addEventListener("open", () => {
+    console.log ("Connected to Server 🌕");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected to Server 🌑");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000); // backend와 메시지를 주고받음 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,8 +7,23 @@ const app = express();
 app.set("view engine","pug");
 app.set("views",__dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000/');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); //http 서버
+const wss = new WebSocket.Server({ server }); //ws 서버
+//서버에서 http, webSocket 둘다 작동시킴
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser 🌕");
+    socket.on("close", () => console.log("Disconnected from the Browser 🌑"));
+    socket.on('message', message => {
+        const translatedMessageData = message.toString('utf8'); // 정수형으로 바꿔줌
+        console.log(translatedMessageData);
+      }); // frontend와 메시지를 주고받음
+    socket.send("hello!!!");
+  });
+
+server.listen(3000, handleListen);
