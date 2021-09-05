14a0ae02e8feed3d13d3abb729a525cc7052f224
DoYounBang <82746064+DoYounBang@users.noreply.github.com>
2021-09-05 15:34:36 +0900

    Ninth commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,12 +3,14 @@ const socket = io();
 const welcome = document.getElementById("welcome")
 const form = welcome.querySelector("form")
 
+function backendDone(mes){
+    console.log(`The backend says: `, mes);//front-end 에서 코드를 만들어 back-end에서 코드를 실행함
+}
+
 function handleRoomSubmit(event){
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", { payload: input.value }, () => {
-        console.log("sever is done!");
-    });
+    socket.emit("enter_room", { payload: input.value },backendDone);// 순서 = event name, 내 마음대로 , function
     input.value = ""; //서버로 부터 실행되는 function을 보냄, emit와 on은 같음 그래서 이름도 같아야 함.
 }
 
--- src/server.js
+++ src/server.js
@@ -17,11 +17,11 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) =>{
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-        done()
-    }, 10000);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
+    setTimeout(() =>{
+      done("hello form the backend");
+    }, 15000);//front-end의 코드를 실행시킴.
   });
 }); //Jsom object를 보냄
 
