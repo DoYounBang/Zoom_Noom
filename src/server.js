8a71d1d135ff85ed3bdcb01cce3c96424bfb443c
DoYounBang <82746064+DoYounBang@users.noreply.github.com>
2021-09-09 16:50:40 +0900

    The 14th one commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import SocketIO from "socket.io";
+import {Server} from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 // import WebSocket from "ws";
 import express from "express";
 
@@ -14,7 +15,15 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log('Listening on http://localhost:3000/');
 
 const httpServer = http.createServer(app); 
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true
+  }
+});
+instrument(wsServer,{
+  auth: false
+});
 
 function publicRooms(){
   const {
@@ -31,6 +40,10 @@ function publicRooms(){
   return publicRooms;
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "익명";
   socket.onAny((event) => {
@@ -39,7 +52,7 @@ wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome", socket.nickname);
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
     wsServer.sockets.emit("room_change", publicRooms());//모두에게 메시지를 보냄
     /*console.log(socket.id);
     console.log(socket.rooms); // Set { <socket.id> }
@@ -47,7 +60,7 @@ wsServer.on("connection", (socket) => {
   });//front-end의 코드를 실행시킴.
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) => 
-      socket.to(room).emit("bye", socket.nickname)
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) -1)
       );
   });
   socket.on("disconnect", () => {
@@ -85,4 +98,5 @@ wss.on("connection", (socket) => {
     });
   });
  */
+
 httpServer.listen(3000, handleListen);
